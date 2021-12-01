ef0a0d8a6de908e417e8b89a44e7f2ae2cd3bb04
dtoms0224 <donnie.toms@phoenix.edu>
2021-12-01 11:51:33 -0600

    commit to master

--- ssg.py
+++ ssg.py
@@ -1,10 +1,11 @@
 import typer 
-
 from ssg.site import site
+
 
 def main(source="content", dest="dist"):
     config = {"source": source, "dest": dest}
 
     site(**config).build()
+
 
 typer.run(main)
--- ssg/site.py
+++ ssg/site.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 
+
 class site:  
     def __init__(self, source, dest):
         self.source = Path(source)
