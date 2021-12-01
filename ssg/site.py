3f74d0e94b653ac59f6af489e971b2482ca79318
dtoms0224 <donnie.toms@phoenix.edu>
2021-12-01 11:41:02 -0600

    commit to master

--- ssg/site.py
+++ ssg/site.py
@@ -1,6 +1,6 @@
 from pathlib import Path
 
-class site:
-    
-    create_dir(self, path):
-        directory = ''
+class site:  
+    def create_dir(self, path):
+        directory = self.dest / path.relative_to(self.source)
+        directory.mkdir(parents=True, exist_ok=True)
